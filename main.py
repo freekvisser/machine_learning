ac995be82f5f3952ff45c2bc238f0c2fd7b91a8b
Melroy Krebbekx <58424603+0982689@users.noreply.github.com>
2022-05-27 13:28:58 +0200

    Changed names and comments. Bruh

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ from neural_network import Network
 from layer import FCLayer, ActivationLayer
 
 
-def mse(y_true, y_pred):
+def loss(y_true, y_pred):
     return np.mean(np.power(y_true - y_pred, 2))
 
 
-def mse_prime(y_true, y_pred):
+def loss_prime(y_true, y_pred):
     return 2 * (y_pred - y_true) / y_true.size
 
 
-def tanh(x):
+def activation(x):
     return np.tanh(x)
 
 
-def tanh_prime(x):
+def activation_prime(x):
     return 1 - np.tanh(x) ** 2
 
 
 if __name__ == '__main__':
     # training data
-    x_train = np.array([[[0, 1, 0,
-                          1, 0, 1,
-                          0, 1, 0]],
-                        [[1, 0, 1,
-                          0, 1, 0,
-                          1, 0, 1]],
-                        [[0, 1, 0,
-                          1, 0, 1,
-                          0, 1, 0]],
-                        [[1, 0, 1,
-                          0, 1, 0,
-                          1, 0, 1]]])
-    y_train = np.array([[[0]],
-                        [[1]],
-                        [[0]],
-                        [[1]]])
+    in_train = np.array([[[0, 1, 0,
+                           1, 0, 1,
+                           0, 1, 0]],
+                         [[1, 0, 1,
+                           0, 1, 0,
+                           1, 0, 1]],
+                         [[0, 1, 0,
+                           1, 0, 1,
+                           0, 1, 0]],
+                         [[1, 0, 1,
+                           0, 1, 0,
+                           1, 0, 1]]])
+    out_train = np.array([[[0]],
+                          [[1]],
+                          [[0]],
+                          [[1]]])
 
     test_cross = np.array([[[0, 1, 0,
                              1, 0, 1,
@@ -46,15 +46,15 @@ if __name__ == '__main__':
                              1, 0, 1]]])
     # network
     net = Network()
-    net.add(FCLayer(9, 3))
-    net.add(ActivationLayer(tanh, tanh_prime))
-    net.add(FCLayer(3, 1))
-    net.add(ActivationLayer(tanh, tanh_prime))
+    net.add_layer(FCLayer(9, 3))
+    net.add_layer(ActivationLayer(activation, activation_prime))
+    net.add_layer(FCLayer(3, 1))
+    net.add_layer(ActivationLayer(activation, activation_prime))
 
     # train
-    net.use(mse, mse_prime)
-    net.fit(x_train, y_train, epochs=1000, learning_rate=0.1)
+    net.create_loss(loss, loss_prime)
+    net.train_network(in_train, out_train, cycles=1000, learning_rate=0.1)
 
     # test
-    out = net.predict(test_cross)
+    out = net.get_output(test_cross)
     print(out)
--- neural_network.py
+++ neural_network.py
@@ -4,24 +4,24 @@ class Network:
         self.loss = None
         self.loss_prime = None
 
-    # add layer to network
-    def add(self, layer):
+    # add layer to neural network
+    def add_layer(self, layer):
         self.layers.append(layer)
 
-    # set loss to use
-    def use(self, loss, loss_prime):
+    # create loss
+    def create_loss(self, loss, loss_prime):
         self.loss = loss
         self.loss_prime = loss_prime
 
-    # predict output for given input
-    def predict(self, input_data):
-        # sample dimension first
+    # train output
+    def get_output(self, input_data):
+        # dimensions input data
         samples = len(input_data)
         result = []
 
-        # run network over all samples
+        # for each sample in range of samples
         for i in range(samples):
-            # forward propagation
+            # do forward propagation
             output = input_data[i]
             for layer in self.layers:
                 output = layer.forward_propagation(output)
@@ -29,28 +29,28 @@ class Network:
 
         return result
 
-    # train the network
-    def fit(self, x_train, y_train, epochs, learning_rate):
-        # sample dimension first
-        samples = len(x_train)
+    # train the neural network
+    def train_network(self, in_train, out_train, cycles, learning_rate):
+        # dimensions of samples
+        samples = len(in_train)
 
-        # training loop
-        for i in range(epochs):
-            err = 0
+        # start training
+        for i in range(cycles):
+            total_error = 0
             for j in range(samples):
-                # forward propagation
-                output = x_train[j]
+                # do forward propagation
+                output = in_train[j]
                 for layer in self.layers:
                     output = layer.forward_propagation(output)
 
                 # compute loss (for display purpose only)
-                err += self.loss(y_train[j], output)
+                total_error += self.loss(out_train[j], output)
 
-                # backward propagation
-                error = self.loss_prime(y_train[j], output)
+                # do backward propagation
+                part_error = self.loss_prime(out_train[j], output)
                 for layer in reversed(self.layers):
-                    error = layer.backward_propagation(error, learning_rate)
+                    part_error = layer.backward_propagation(part_error, learning_rate)
 
             # calculate average error on all samples
-            err /= samples
-            print('epoch %d/%d   error=%f' % (i+1, epochs, err))
+            total_error /= samples
+            print('range %d/%d   error_value=%f' % (i + 1, cycles, total_error))
