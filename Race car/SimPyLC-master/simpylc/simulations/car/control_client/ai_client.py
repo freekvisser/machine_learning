46da4067a538ec5f39387157a6bee7fee6c3439d
Melroy Krebbekx <0982689@hr.nl>
2022-06-22 14:16:33 +0200

    removed neural network

--- Race car/SimPyLC-master/simpylc/simulations/car/control_client/ai_client.py
+++ Race car/SimPyLC-master/simpylc/simulations/car/control_client/ai_client.py
@@ -8,24 +8,6 @@ ss.path += [os.path.abspath(relPath) for relPath in ('..',)]
 import socket_wrapper as sw
 import parameters as pm
 
-from neural_network import Network
-from layer import FCLayer, ActivationLayer
-
-def loss(y_true, y_pred):
-    return np.mean(np.power(y_true - y_pred, 2))
-
-
-def loss_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / y_true.size
-
-
-def activation(x):
-    return np.tanh(x)
-
-
-def activation_prime(x):
-    return 1 - np.tanh(x) ** 2
-
 class AiClient:
     def __init__(self):
         self.steeringAngle = 0
@@ -90,41 +72,3 @@ class AiClient:
 
 if __name__ == '__main__':
     AiClient()
-
-    in_train = np.array([[[0, 1, 0,
-                           1, 0, 1,
-                           0, 1, 0]],
-                         [[1, 0, 1,
-                           0, 1, 0,
-                           1, 0, 1]],
-                         [[0, 1, 0,
-                           1, 0, 1,
-                           0, 1, 0]],
-                         [[1, 0, 1,
-                           0, 1, 0,
-                           1, 0, 1]]])
-    out_train = np.array([[[0]],
-                          [[1]],
-                          [[0]],
-                          [[1]]])
-
-    test_cross = np.array([[[0, 1, 0,
-                             1, 0, 1,
-                             0, 1, 0],
-                            [1, 0, 1,
-                             0, 1, 0,
-                             1, 0, 1]]])
-    # network
-    net = Network()
-    net.add_layer(FCLayer(9, 3))
-    net.add_layer(ActivationLayer(activation, activation_prime))
-    net.add_layer(FCLayer(3, 1))
-    net.add_layer(ActivationLayer(activation, activation_prime))
-
-    # train
-    net.create_loss(loss, loss_prime)
-    net.train_network(in_train, out_train, cycles=1000, learning_rate=0.1)
-
-    # test
-    out = net.get_output(test_cross)
-    print(out)
